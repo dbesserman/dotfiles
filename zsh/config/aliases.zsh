54088151ca31efa67a02fee1a533bc57f2baa102
David Besserman <david.besserman@gmail.com>
2022-11-29 17:27:44 +0100

    [zsh] fix zshrc import issue and add git aliases

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,9 +1,14 @@
-alias vi=nvim
-alias gfz='git checkout $(git branch | fzf)'
-alias phs='source ./config/.env && iex -S mix phx.server'
-alias gh='git show -s --format=%H | pbcopy'
-alias gcp='g cherry-pick `pbpaste`'
+# git
 alias g='git'
 alias gaa='git add .'
 alias gco='git checkout'
 alias gst='git status'
+alias gh='git show -s --format=%H | pbcopy'
+alias gcp='git cherry-pick `pbpaste`'
+alias gfz='git checkout $(git branch | fzf)'
+
+# vim
+alias vi=nvim
+
+# elixir
+alias phs='source ./config/.env && iex -S mix phx.server'
